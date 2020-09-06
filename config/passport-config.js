5b358b30912f8503a1d62f59dcafb4310d6e2dde
Mohamed <mohamedelshazly18@yahoo.com>
2020-09-06 20:43:05 +0200

    callback url changed

--- config/passport-config.js
+++ config/passport-config.js
@@ -19,7 +19,7 @@ passport.use(
     new GoogleStrategy({
         clientID : process.env.GOOGLE_CLIENT_ID, 
         clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL : 'http://localhost:3000/auth/google/redirect'
+        callbackURL : '/auth/google/redirect'
     }, 
     (accessToken, refreshToken, profile, done) => {
         User.findOne({googleID : profile.id}).then((currentUser) => {
